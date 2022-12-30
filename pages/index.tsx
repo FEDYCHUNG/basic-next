c09b449516423c071f58866e736d78b3e2c115d4
FEDYCHUNG <fedychung@gmail.com>
2022-12-31 05:47:31 +0700

    add header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,10 @@
-import Head from "next/head";
-import Image from "next/image";
-import Link from "next/link";
-// import { Inter } from '@next/font/google'
-// import styles from '../styles/Home.module.css'
-
-// const inter = Inter({ subsets: ['latin'] })
+import Header from "../components/Header";
 
 export default function Home() {
   return (
     <>
-      <header>
-        <ul>
-          <li>
-            <Link href="/">Home</Link>
-            <Link href="/blog">Blog</Link>
-            <Link href="/users">Users</Link>
-          </li>
-        </ul>
-      </header>
+      <Header />
+      <h1>Welcome</h1>
     </>
   );
 }
