e51298499f35f834e29f460565254b481538dac3
FEDYCHUNG <fedychung@gmail.com>
2023-01-08 13:11:04 +0700

    Next Image

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
+import Image from "next/image";
 import Layout from "../components/Layout";
 
 export default function Home() {
   return (
     <Layout pageTitle="Home Page">
+      <Image src="/../public/images/profile.png" width={200} height={200} alt="profile" />
       <h1>Welcome</h1>
     </Layout>
   );
