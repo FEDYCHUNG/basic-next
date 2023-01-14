72971edd9058c800aec84c833299972b94e52f7a
FEDYCHUNG <fedychung@gmail.com>
2023-01-14 18:22:55 +0700

    getStaticPath

--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -1,4 +1,6 @@
+import { useRouter } from "next/router";
 import Layout from "../../components/Layout";
+import styles from "../../styles/Users.module.css";
 
 interface UsersProps {
   dataUsers: Array<any>;
@@ -6,13 +8,12 @@ interface UsersProps {
 
 export default function Users(props: UsersProps) {
   const { dataUsers } = props;
-
-  console.log(dataUsers);
+  const router = useRouter();
 
   return (
     <Layout pageTitle="Users Page">
       {dataUsers.map((user) => (
-        <div key={user.id}>
+        <div key={user.id} onClick={() => router.push(`/users/${user.id}`)} className={styles.card}>
           <p>{user.name}</p>
           <p>{user.email}</p>
         </div>
